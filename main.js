ed5544445547168a4497f14f505162a05341985c
fatimamassou <massoufatima23@gmail.com>
2025-10-11 12:41:40 +0000

    make inputs empty after submit the form .

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const header = document.querySelector('header');
+const contactForm = document.querySelector('#contact-form');
+const nameInput = document.querySelector('.name');
+const emailInput = document.querySelector('.email');
+const messageInput = document.querySelector('.message');
+
 window.addEventListener("scroll",function (){
     header.classList.toggle ("sticky" , this.window.scrollY > 120);
 });
+
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 menu.onclick = () => {
@@ -12,3 +18,23 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('active');
 }
+
+contactForm.addEventListener('submit', async function(event) {
+  event.preventDefault(); // stop auto reload
+  const formData = new FormData(contactForm);
+
+  const response = await fetch(contactForm.action, {
+    method: contactForm.method,
+    body: formData,
+    headers: { 'Accept': 'application/json' }
+  });
+
+  if (response.ok) {
+    alert('Message sent successfully!');
+    nameInput.value = '';
+    emailInput.value = '';
+    messageInput.value = '';
+  } else {
+    alert('Oops! Something went wrong.');
+  }
+});
