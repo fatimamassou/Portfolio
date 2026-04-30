06902af5b739256dee79bb82bdb4d3af8784c47c
fatimamassou <153992769+fatimamassou@users.noreply.github.com>
2026-04-30 12:06:40 +0100

    lint

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ window.addEventListener("scroll",function (){
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
+
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('active');
